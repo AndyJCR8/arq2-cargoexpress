e2c03baa2679bbca4a5611c8e5d2d7bf6c293cb8
Ammi González <64441015+AmmiGonzalez@users.noreply.github.com>
2023-02-25 21:29:28 -0600

    Creación de tablas de datos
    Diseño de las tablas que despliegan los datos de clientes y usuarios para realizar CRUD.

--- frontend/frontend-express/src/Pages/Home.jsx
+++ frontend/frontend-express/src/Pages/Home.jsx
@@ -20,6 +20,37 @@ function ClientsView() {
   return (
     <div>
       Vista para ciertos usuarios como secretarias
+      {/* Las secretarias modifican los clientes */}
+      <table class="table table-striped table-hove">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Nombre</th>
+          <th scope="col">NIT</th>
+          <th scope="col">Dirección</th>
+          <th scope="col">Teléfono</th>
+          <th scope="col">Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+          <td>Mark</td>
+          <td>Otto</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+        </tr>
+      </tbody>
+      </table>
     </div>
   )
 }
@@ -28,6 +59,43 @@ function UsersView() {
   return (
     <div>
       Vista para administradores
+      {/* Los administradores modifican los usuarios */}
+      <table class="table table-striped table-hove">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Usuario</th>
+          <th scope="col">Nombre</th>
+          <th scope="col">Email</th>
+          <th scope="col">Teléfono</th>
+          <th scope="col">Contraseña</th>
+          <th scope="col">Oficina</th>
+          <th scope="col">Rol</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+          <td>Otto</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+          <td>Thornton</td>
+        </tr>
+      </tbody>
+      </table>
     </div>
   )
 }
